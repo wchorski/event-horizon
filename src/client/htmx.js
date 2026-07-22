6cfa193d31711fb6e02fc16d3f7f33f0edc069c8
wchorski <wchorski@gmail.com>
2026-07-21 19:43:51 -0500

    sql migration

--- src/client/htmx.js
+++ src/client/htmx.js
@@ -3,6 +3,9 @@ import "htmx-ext-response-targets";
 import { PUBLIC_HTMX_LOGS } from "astro:env/client";
 const isDev = import.meta.env.DEV;
 
+console.log("will astro:env/client work in this client/htmx.js script?");
+console.log({ PUBLIC_HTMX_LOGS });
+
 if (isDev && PUBLIC_HTMX_LOGS) {
   // Enable logging in development mode
   htmx.logAll();

492bbacada7ae5492a80d1b9182553ed433187ce
wchorski <wchorski@gmail.com>
2026-09-24 15:42:26 -0500

    middleware to all protected routes w exclusion of some public routes. added context.locals.organization

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,28 +22,39 @@ export const onRequest = defineMiddleware(async (context, next) => {
   context.locals.user = session?.user ?? null;
   context.locals.session = session?.session ?? null;
 
-  const protectedRoutes = [
-    "/documents",
-    "/admin",
-    "/dashboard",
-    "/partials/events",
-    "/partials/users",
-    "/partials/organizations",
-    "/partials/organizations",
-    "/partials/tickets",
-  ];
+  const publicRoutes = ["/login", "/sign-up", "/forgot-password"];
+  const isPublic = publicRoutes.some((route) => context.url.pathname.startsWith(route));
 
-  const requiresAuth = protectedRoutes.some((route) =>
-    context.url.pathname.startsWith(route),
-  );
-
-  if (requiresAuth && !context.locals.user) {
+  if (!isPublic && !context.locals.user) {
     return context.redirect(
       `/login?returnTo=${encodeURIComponent(
         context.url.pathname + context.url.search,
       )}&msg=${encodeURIComponent("Unauthorized. Please login to be returned back to the previous page")}`,
     );
   }
+  //? all routes are protected unless made open (above)
+  // const protectedRoutes = [
+  //   "/documents",
+  //   "/admin",
+  //   "/dashboard",
+  //   "/partials/events",
+  //   "/partials/users",
+  //   "/partials/organizations",
+  //   "/partials/organizations",
+  //   "/partials/tickets",
+  // ];
+
+  // const requiresAuth = protectedRoutes.some((route) =>
+  //   context.url.pathname.startsWith(route),
+  // );
+
+  // if (requiresAuth && !context.locals.user) {
+  //   return context.redirect(
+  //     `/login?returnTo=${encodeURIComponent(
+  //       context.url.pathname + context.url.search,
+  //     )}&msg=${encodeURIComponent("Unauthorized. Please login to be returned back to the previous page")}`,
+  //   );
+  // }
 
   if (isDev) {
     return next();
